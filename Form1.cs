bacd19b19ad4b8064887c74f72ff19bf471cff99
Usuario <diegodarwin@mesoamericana.edu.gt>
2021-03-11 13:03:49 -0600

    Creación de la función guardar los datos en los archivos

--- Form1.cs
+++ Form1.cs
@@ -58,6 +58,38 @@ namespace Propiedades_Condominio
             //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
             reader2.Close();
         }
+
+        private void Guardar()
+        {
+            //Abrir el archivo: Write sobreescribe el archivo, Append agrega los datos al final del archivo
+            FileStream stream = new FileStream("Propietarios.txt", FileMode.Create, FileAccess.Write);
+            //Crear un objeto para escribir el archivo
+            StreamWriter writer = new StreamWriter(stream);
+            //Usar el objeto para escribir al archivo, WriteLine, escribe linea por linea
+            //Write escribe todo en la misma linea. En este ejemplo se hará un dato por cada línea
+            foreach (var p in listaPropietarios)
+            {
+                writer.WriteLine(p.Dpi);
+                writer.WriteLine(p.Nombre);
+                writer.WriteLine(p.Apellido);
+            }
+            //Cerrar el archivo
+            writer.Close();
+
+            FileStream stream2 = new FileStream("Propiedades.txt", FileMode.Create, FileAccess.Write);
+            //Crear un objeto para escribir el archivo
+            StreamWriter writer2 = new StreamWriter(stream2);
+            //Usar el objeto para escribir al archivo, WriteLine, escribe linea por linea
+            //Write escribe todo en la misma linea. En este ejemplo se hará un dato por cada línea
+            foreach (var p in listaPropiedades)
+            {
+                writer2.WriteLine(p.No_Casa);
+                writer2.WriteLine(p.Dpi);
+                writer2.WriteLine(p.Cuota_Mantenimiento);
+            }
+            //Cerrar el archivo
+            writer2.Close();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             leer();
