12f69d644dcb2f7d24c54a7bdbf0f4bc5bca3602
Usuario <diegodarwin@mesoamericana.edu.gt>
2021-03-11 12:55:40 -0600

    Creación de clases de los datos propietarios y propiedades, y también la función leer

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,54 @@ namespace Propiedades_Condominio
 {
     public partial class Form1 : Form
     {
+        List<Propiedades> listaPropiedades = new List<Propiedades>();
+        List<Propietarios> listaPropietarios = new List<Propietarios>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void leer()
+        {
+            FileStream stream = new FileStream("Propietarios.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+
+            while (reader.Peek() > -1)
+            {
+                Propietarios Temp = new Propietarios();
+
+                Temp.Dpi = reader.ReadLine();
+                Temp.Nombre= reader.ReadLine();
+                Temp.Apellido = reader.ReadLine();
+                
+
+                listaPropietarios.Add(Temp);
+
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader.Close();
+
+            FileStream stream2 = new FileStream("Propiedades.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader2 = new StreamReader(stream2);
+
+            while (reader2.Peek() > -1)
+            {
+                Propiedades Temp = new Propiedades();
+
+                Temp.No_Casa = Convert.ToInt16(reader2.ReadLine());
+                Temp.Dpi = reader2.ReadLine();
+                Temp.Cuota_Mantenimiento = Convert.ToInt16(reader2.ReadLine());
+       
+
+                listaPropiedades.Add(Temp);
+
+            }
+            //Cerrar el archivo, esta linea es importante porque sino despues de correr varias veces el programa daría error de que el archivo quedó abierto muchas veces. Entonces es necesario cerrarlo despues de terminar de leerlo.
+            reader2.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            leer();
         }
     }
 }
---
+++ Propiedades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propiedades_Condominio
+{
+    class Propiedades
+    {
+        int no_Casa;
+        string dpi;
+        int cuota_Mantenimiento;
+
+        public int No_Casa { get => no_Casa; set => no_Casa = value; }
+        public string Dpi { get => dpi; set => dpi = value; }
+        public int Cuota_Mantenimiento { get => cuota_Mantenimiento; set => cuota_Mantenimiento = value; }
+    }
+}
