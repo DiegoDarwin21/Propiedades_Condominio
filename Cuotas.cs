074f78afdf15341df9c518c903ff9e1bdaf008b6
Usuario <diegodarwin@mesoamericana.edu.gt>
2021-03-11 14:55:38 -0600

    Creación de clase que contiene las propiedades que se mostraran en el gridview

---
+++ Cuotas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propiedades_Condominio
+{
+    class Cuotas
+    {
+        string nombre;
+        string apellido;
+        int no_CAsa;
+        int cuto_Mantenimientos;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int No_CAsa { get => no_CAsa; set => no_CAsa = value; }
+        public int Cuto_Mantenimientos { get => cuto_Mantenimientos; set => cuto_Mantenimientos = value; }
+    }
+}
